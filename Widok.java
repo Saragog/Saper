de56ab2e00ca23325ebb90e898a328b11d0e3fec
Saragog <andrzejdackiewicz@gmail.com>
2014-12-28 03:07:58 +0100

    Create Widok.java

---
+++ Widok.java
@@ -0,0 +1,196 @@
+package View;
+
+
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class Widok extends JFrame// implements ActionListener
+{
+
+	private mojJButton[] przyciski;
+	private int ilePoziomo;
+	private int ilePionowo;
+
+	public Widok()				// konstruktor domyslny
+	{
+		setSize(200,229); // poziomo | pionowo     | ramka na gorze zajmuje doklanie 29 jednostek
+		setTitle("Saper");
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		przyciski = new mojJButton[25];
+		
+		ilePoziomo = 5;
+		ilePionowo = 5;
+		
+		int x;
+		int y;
+		
+		for (int i = 0 ; i < 25 ; i++)
+		{
+			przyciski[i] = new mojJButton(i);
+		}
+		for (y = 0; y < 5; y++)
+		{
+			for (x = 0 ; x < 5; x++)
+			{
+				przyciski[x + y*5].setBounds(x*40, y*40, 40, 40);
+				add (przyciski[x + y*5]);
+
+			}
+			x = 0;
+		}
+	}
+	public Widok(int ileK, int ileS)	// ile Kolumn ile Szeregow
+	{
+		przyciski = new mojJButton[ileK*ileS];
+		
+		setSize(40*ileK,29+40*ileS);
+		setTitle("Saper");
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ilePoziomo = ileK;
+		ilePionowo = ileS;
+		
+		int x;
+		int y;
+		
+		for (int i = 0 ; i < ileK*ileS ; i++)
+		{
+			przyciski[i] = new mojJButton(i);
+		}
+		for (y = 0; y < ileS; y++)
+		{
+			for (x = 0 ; x < ileK; x++)
+			{
+				przyciski[x + y*ileK].setBounds(x*40, y*40, 40, 40);		// distance to right(ile w prawo) | distance down(ile w dol) | width(szerokosc) | height(wysokosc)
+				add (przyciski[x + y*ileK]);
+				
+			}
+			x = 0;
+		}
+		
+	}
+
+	public void AktualizujWidok(int [] tablicaZawartosci,int[] tablicaOdsloniec)
+	{
+		System.out.println(tablicaOdsloniec.length);
+		
+		for (int x = 0; x < tablicaOdsloniec.length;x++)
+		{
+			System.out.println("usuwam" + tablicaOdsloniec[x]);
+			remove(przyciski[tablicaOdsloniec[x]]);
+			validate();
+			repaint();
+			
+			int kolumna = tablicaOdsloniec[x] % ilePoziomo;
+			int szereg = tablicaOdsloniec[x] / ilePoziomo;
+			
+			System.out.println("Wstawic trzeba JLabel w kolumne: " + kolumna + " i szereg: " + szereg);
+			
+			JLabel nowyLabel;
+			
+			switch (tablicaZawartosci[x])
+			{
+				case 1:										// 1 mina wokol
+				{
+					nowyLabel = new JLabel("    1   ");
+					break;
+				}
+				case 2:										// 2 miny wokol
+				{
+					nowyLabel = new JLabel("    2   ");
+					break;
+				}
+				case 3:										// 3 miny wokol
+				{
+					nowyLabel = new JLabel("    3   ");
+					break;
+				}
+				case 4:										// 4 miny wokol
+				{
+					nowyLabel = new JLabel("    4   ");
+					break;
+				}
+				case 5:										// 5 min wokol
+				{
+					nowyLabel = new JLabel("    5   ");
+					break;
+				}
+				case 6:										// 6 min wokol
+				{
+					nowyLabel = new JLabel("    6   ");
+					break;
+				}
+				case 7:										// 7 min wokol
+				{
+					nowyLabel = new JLabel("    7   ");
+					break;
+				}
+				case 8:										// 8 min wokol
+				{
+					nowyLabel = new JLabel("    8   ");
+					break;
+				}
+				case 9:										// pole jest mina
+				{
+					nowyLabel = new JLabel("    M   ");
+					break;
+				}
+				default:										// pole jest puste
+				{
+					nowyLabel = new JLabel("    P   ");
+				}
+			}
+			
+			nowyLabel.setBounds(kolumna*40, szereg*40, 40, 40);
+			add (nowyLabel);
+			
+			
+			// dodanie JLabel w to samo miejsce
+		}
+	
+	}
+	
+/*
+	public void actionPerformed(ActionEvent e)
+	{
+		JButton przycisk = (JButton)(e.getSource());
+		
+		System.out.println("Wcisnieto przycisk");
+		
+
+		for(int i = 0; i < przyciski.length;i++)
+		{
+			if ( przyciski[i] == przycisk)
+			{
+				//System.out.println( "Wlasnie znalazlem index : " + i );	// znajduje index w tablicy wcisnietego przycisku
+				number = i;
+				break;
+			}
+		}
+	}
+	
+	public int getNumber ()
+	{
+		//System.out.println(number);
+		return number;
+		
+	}
+*/
+	
+	
+	public void addListener (int ktoryPrzycisk, ActionListener listenerDlaPrzycisku)
+	{
+		przyciski[ktoryPrzycisk].addActionListener(listenerDlaPrzycisku);
+	}
+	
+	
+	public void displayErrorMessage(String errorMessage)
+	{
+		JOptionPane.showMessageDialog(this, errorMessage);
+	}
+}
